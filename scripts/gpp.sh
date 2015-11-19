60b3530ac2048e48b0cc4a6fc328c9ff1f913488
Thaiane <thaianefbraga@gmail.com>
2015-11-19 03:25:52 -0200

    Simplificando script
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>
    Signed-off-by: Thaiane Braga <thaianefbraga@gmail.com>

--- scripts/gpp.sh
+++ scripts/gpp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing: ( 1 ) g++, ( 2 ) flex and ( 3 ) bison"
+echo "Installing: ( 1 ) g++, ( 2 ) flexc++ and ( 3 ) bisonc++"
 
 # Install g++:
 #   g++ is a compiler to C++
@@ -9,47 +9,12 @@ echo "Install g++: starting"
 sudo apt-get install g++ -y >>/tmp/provision-script.log 2>&1
 echo "Install g++: DONE"
 
-# Install flex:
-#   Recognize lexical standards
-#
-echo "Install flex: starting"
-sudo apt-get install flex -y >>/tmp/provision-script.log 2>&1
-echo "Install flex: DONE"
-
-# Install flex++:
-#   Recognize lexical standards and create a scanner in C++
-#
-echo "Install flex++: starting"
-sudo apt-get install flex++ -y >>/tmp/provision-script.log 2>&1
-echo "Install flex++: DONE"
-
-# Install flex++:
-#   Recognize lexical standards and create a scanner in C++
-#
-echo "Install flexc++: starting"
-sudo apt-get install flexc++ -y >>/tmp/provision-script.log 2>&1
-echo "Install flexc++: DONE"
-
 # Install flexc++:
 #   Recognize lexical standards and create a scanner in C++
 #
 echo "Install flexc++: starting"
 sudo apt-get install flexc++ -y >>/tmp/provision-script.log 2>&1
 echo "Install flexc++: DONE"
-
-# Install bison:
-#   Generator of interpreter of languages
-#
-echo "Install bison: starting"
-sudo apt-get install bison -y >>/tmp/provision-script.log 2>&1
-echo "Install bison: DONE"
-
-# Install bison++:
-#   Generator of interpreter of languages and create a parser in C++
-#
-echo "Install bison++: starting"
-sudo apt-get install bison++ -y >>/tmp/provision-script.log 2>&1
-echo "Install bison++: DONE"
 
 # Install bisonc++:
 #   Generator of interpreter of languages and create a parser in C++
