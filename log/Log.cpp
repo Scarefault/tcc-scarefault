f8b0b68402f16912d9fdd1af4f09c95516577a1d
Thaiane <thaianefbraga@gmail.com>
2016-03-15 00:59:51 +0000

    Implementando função de Data e Hora para o log
    A partir dessa função é possível retornar a
      data e hora de uma determinada mensagem.
    
    [issue: Scarefault/Management-Central#17]
    
    Signed-off-by: Thaiane Braga <thaianefbraga@gmail.com>
    Signed-off-by: Tomaz Martins <tomaz.r.martins@gmail.com>

--- log/Log.cpp
+++ log/Log.cpp
@@ -20,13 +20,8 @@ void Log::info( std::string message )
     time_t rawtime;
     time( &rawtime );
 
-    std::cout << ctime(&rawtime) << std::endl;
-
     const unsigned int size_message = sizeof(char)*message.size();
     const unsigned int size_time = sizeof( ctime( &rawtime ) );
-
-    log_stream_ptr->write( ctime( &rawtime ), size_time );
-    log_stream_ptr->write( " - ", 3 );
 
     log_stream_ptr->write( message.c_str(), size_message );
     log_stream_ptr->write( "\n", 2 );
@@ -36,6 +31,15 @@ void Log::info( std::string message )
   }
 
   log_stream_ptr->close();
+}
+
+char* Log::get_date_time( char* buffer )
+{
+  time_t t = time( 0 );
+  strftime( buffer, date_time_size, date_time_format.c_str(), 
+            localtime( &t ) );
+
+  return buffer;
 }
 
 std::fstream* Log::get_log_stream()
--- log/Log.hpp
+++ log/Log.hpp
@@ -6,8 +6,12 @@
 #define WRITE std::fstream::out
 #define APPEND std::fstream::app
 
+
 #include <fstream>
 #include <string>
+
+const std::string date_time_format( "%d %B %Y%t%a%t%T - " );
+const int date_time_size = 50;
 
 class Log
 {
@@ -17,8 +21,10 @@ class Log
     void info( std::string );
 
   private:
+    char buffer[date_time_size];
     std::fstream log_stream;
 
+    char* get_date_time( char* );
     std::fstream* get_log_stream();
 };
 
